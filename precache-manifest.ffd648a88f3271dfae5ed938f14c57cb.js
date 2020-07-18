ea5e3c7bc6c477ef94c792bf013a6f8c6e02962b
mithileshrprajapati <mithileshrprajapati@gmail.com>
2020-07-18 21:04:33 +0530

    Updates

---
+++ precache-manifest.ffd648a88f3271dfae5ed938f14c57cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2054b86a03c38e895c88f08a8d905ba5",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e50cbd8113fab7a4d156",
+    "url": "/monster-rolodex/static/css/main.11ed2098.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e50cbd8113fab7a4d156",
+    "url": "/monster-rolodex/static/js/main.d8593d42.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
